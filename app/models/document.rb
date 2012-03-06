5d14cd97c7c24136163c6503b71d2e4f41fc1f96
Clément Roullet <clement.roullet@gmail.com>
2012-03-06 11:16:12 +0100

    Changed PaperClip storage path

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,7 +5,7 @@ class Document < ActiveRecord::Base
   #set up "uploaded_file" field as attached_file (using Paperclip)
   has_attached_file :uploaded_file,
                :url => "/assets/get/:id",
-               :path => ":Rails_root/assets/:id/:basename.:extension"
+               :path => ":rails_root/public/system/:id/:basename.:extension"
   validates_attachment_size :uploaded_file, :less_than => 10.megabytes
   validates_attachment_presence :uploaded_file
 
