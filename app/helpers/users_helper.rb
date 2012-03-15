c9fb98d1df86853670056c8e43a6c7e362c2764e
Clément Roullet <clement.roullet@gmail.com>
2012-03-15 12:18:34 +0100

    Administration completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,15 @@
 # encoding: UTF-8
 module UsersHelper
+  def details_tag (user)
+    return   (user.phone.blank? ? "" : raw("<strong>Tel.</strong> #{user.phone}<br/>")) + \
+              (user.cellphone.blank? ? "" : raw("<strong>Port.</strong> #{user.cellphone}<br/>")) + \
+               (user.fax.blank? ? "" : raw("<strong>Fax.</strong> #{user.fax}<br/>"))
+  end
+
   def address_tag (user)
-    return raw "<strong>%s</strong>" % (user.company.nil? ? "Entreprise non spécifiée" : user.company) + \
-      "<br/>%s<br/>" % (user.street.nil? ? "Rue non spécifiée" : user.street) + \
-      "%s " % (user.zip.nil? ? "CP non spécifié" : user.zip) + \
-      "%s<br/>" % (user.city.nil? ? "Ville non spécifiée" : user.city)
+    return raw "<strong>%s</strong>" % (user.company.blank? ? "Entreprise non spécifiée" : user.company) + \
+      "<br/>%s<br/>" % (user.street.blank? ? "Rue non spécifiée" : user.street) + \
+      "%s " % (user.zip.blank? ? "CP non spécifié" : user.zip) + \
+      "%s<br/>" % (user.city.blank? ? "Ville non spécifiée" : user.city)
   end
 end
